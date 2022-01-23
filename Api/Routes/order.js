90e6bddc359520825acaf649290f083e56c50bcd
Mohamed LOTF <m.lotf@catie.fr>
2022-01-23 16:36:27 +0100

    CRUD Order

--- Api/Routes/order.js
+++ Api/Routes/order.js
@@ -1,4 +1,27 @@
-const router = require("express").Router()
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken")
 
+const router = require("express").Router()
+const Order = require("../Models/Order")
+
+//CREATE
+router.post("/", verifyToken, async (req, res) => {
+    const newOrder = new Order(req.body)
+    try {
+        const savedOrder = await newOrder.save();
+        res.status(200).json(savedOrder)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//GET USER ORDERS
+router.get("/:userId", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const order = await Order.find({ userId: req.params.userId });
+        res.status(200).json(order);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router
