650a480d3e99ef5dba42b923f1cc31fea6b7c556
Mohamed LOTF <m.lotf@catie.fr>
2022-01-26 21:10:52 +0100

    Link categories

--- e-commerce/src/Components/CategoryItem.jsx
+++ e-commerce/src/Components/CategoryItem.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./../styles/categories.css"
 
 function CategoryItem({ item }) {
   return (
     <div className="item-container">
-      <div className="image-container">
-        <img src={item.img} />
-        <div className="info-container">
-          <h2>{item.title}</h2>
-          <button>SHOP NOW</button>
+      <Link style={{ textDecoration: 'none' }} to={`/products/${item.category}`} >
+        <div className="image-container">
+          <img src={item.img} />
+          <div className="info-container">
+            <h2>{item.title}</h2>
+            <button>SHOP NOW</button>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
