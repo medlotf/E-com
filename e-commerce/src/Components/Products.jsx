9a82d1a0374312dd7d7614a9b639818badf5286d
Mohamed LOTF <m.lotf@catie.fr>
2022-01-27 01:12:34 +0100

    fetch products from api

--- e-commerce/src/Components/Products.jsx
+++ e-commerce/src/Components/Products.jsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from "./Product";
 import "./../styles/products.css"
-import { productsItems } from "./../data/productItems"
+import axios from "axios"
 
-function Products(props) {
-    return (
-        <div className="products-container">
-            {productsItems.map((item) => (
-                <Product item={item} key={item.id} />
-            ))}
-        </div>
-    );
+function Products({ category, sort, filters }) {
+
+  const [products, setProducts] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(category ? `http://localhost:9000/api/products?category=${category}` : "http://localhost:9000/api/products");
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err)
+      }
+    };
+    getProducts();
+  }, [category]);
+
+  useEffect(() => {
+    category && setFilteredProducts(products.filter((item) => Object.entries(filters).every(([key, value]) => item[key].includes(value))));
+  }, [products, category, filters]);
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
+  return (
+    <div className="products-container">
+      {category
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+          .slice(0, 8)
+          .map((item) => <Product item={item} key={item.id} />)
+      }
+    </div>
+  );
 }
 
 export default Products;
